c4fbac9dd6350157bcad053619739a1e29cffe73
Max Dieckmann <max.dieckmann@upf.edu>
2022-05-26 23:18:16 +0200

    Preparations for deployment.

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
-
+const compression = require('compression');
+const helmet = require('helmet');
 // Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
 require('dotenv').config();
 const app = express();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+app.use(helmet());
+app.use(compression());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
